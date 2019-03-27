56f9f1799266b8ca6a3109108918893dcfd2d32d
Stephan Kerbl <stephankerbl@gmail.com>
2019-03-27 12:32:20 +0100

    GAMES-001 Welcome screen [SK,RK]

---
+++ app/src/main/java/com/example/ultimategames/WelcomeScreenActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+    public int global_score = 0;
+
+    Button buttonTicTacToe;
+    Button buttonHangman;
+    Button buttonTouchBlock;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        buttonTicTacToe = (Button) findViewById(R.id.btn_tictactoe);
+        buttonHangman = (Button) findViewById(R.id.btn_hangman);
+        buttonTouchBlock = (Button) findViewById(R.id.btn_block);
+
+        buttonTicTacToe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Tic Tac Toe selected.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonHangman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hangman selected.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonTouchBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Touch the Block selected.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        TextView tvScore = (TextView) findViewById(R.id.text_score);
+        String textScore = Integer.toString(global_score);
+
+        tvScore.setText(textScore);
+    }
+}
--- app/src/test/java/com/example/ultimategames/ExampleUnitTest.java
+++ app/src/test/java/com/example/ultimategames/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.ultimategames;
+package com.example.myapplication;
 
 import org.junit.Test;
 
