c2d6d0ec906873d7a89ed75a5c30a235b9027fd0
Markus Schiffermueller <schiffermueller@student.tugraz.at>
2019-03-27 15:27:51 +0100

    Games-011 Changing the button to the correct width and height [AG,MS]

--- app/src/main/java/com/example/ultimategames/MainActivity.java
+++ app/src/main/java/com/example/ultimategames/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ultimategames;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.ultimategames.TouchTheBlock.TTB_Activity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
+++ app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
@@ -1,8 +1,12 @@
 package com.example.ultimategames.TouchTheBlock;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
 import android.widget.Button;
+import android.graphics.Color;
 
 import com.example.ultimategames.R;
 
@@ -14,9 +18,18 @@ public class TTB_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Set the game activity content view!
         setContentView(R.layout.touchtheblock);
+
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        Log.e("Width", "" + width);
+        Log.e("height", "" + height);
         // Todo: Make Layout Ready and set on Click Listener to Block and to the game canvas
-        Button butn = (Button)findViewById(R.id.bt_block);
-        butn.setHeight(300);
-        butn.setWidth(300);
+        Button butn = findViewById(R.id.bt_block);
+        butn.setHeight(height/2);
+        butn.setWidth(width/2);
     }
 }
