8d597bac66ad5d5dde09a412b20f2b5327674932
Markus Schiffermueller <schiffermueller@student.tugraz.at>
2019-03-27 15:00:37 +0100

    Games-011 Making the button size changeable [AG,MS]

--- app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
+++ app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
@@ -2,6 +2,7 @@ package com.example.ultimategames.TouchTheBlock;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.example.ultimategames.R;
 
@@ -14,6 +15,8 @@ public class TTB_Activity extends AppCompatActivity {
         // Set the game activity content view!
         setContentView(R.layout.touchtheblock);
         // Todo: Make Layout Ready and set on Click Listener to Block and to the game canvas
-
+        Button butn = (Button)findViewById(R.id.bt_block);
+        butn.setHeight(300);
+        butn.setWidth(300);
     }
 }
