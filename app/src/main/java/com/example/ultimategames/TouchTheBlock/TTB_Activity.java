bee12be45f22780b686f3ce32c17d46b8d5b4597
Alexander Grossmann <alexander.grossmann@student.tugraz.at>
2019-03-27 11:51:21 +0100

    Games-011 Implemented Game Activity [AG, MS]

---
+++ app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
@@ -0,0 +1,18 @@
+package com.example.ultimategames.TouchTheBlock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ultimategames.R;
+
+public class TTB_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        // Set the game activity content view!
+        setContentView(R.layout.touchtheblock);
+        // Todo: Make Layout Ready and set on Click Listener to Block and to the game canvas
+    }
+}
