09bf5787a5cc822e04e8fea253e5ff6ecd09ac78
Alexander Grossmann <alexander.grossmann@student.tugraz.at>
2019-04-03 15:11:32 +0200

    GAMES-011 Improved Espresso Test [AG, MS]
    Test the clicking 5 times to make this Espresso test more sense full.

---
+++ app/src/test/java/com/example/ultimategames/TTB_UnitTest.java
@@ -0,0 +1,33 @@
+package com.example.ultimategames;
+
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.ultimategames.TouchTheBlock.TTB_Activity;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class TTB_UnitTest {
+    TTB_Activity ttb;
+    @Before
+    public void setUp() throws Exception {
+        ttb = new TTB_Activity();
+    }
+
+    @Test
+    public void pointsShouldBeIncreased() {
+
+
+    }
+
+    @Test
+    public void pointsShouldBeDecreased() {
+
+
+    }
+
+}
