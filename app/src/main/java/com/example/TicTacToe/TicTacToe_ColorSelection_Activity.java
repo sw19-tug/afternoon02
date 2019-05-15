bef049a4b5a7715d53c9845b43595ab0b9aec9ed
Tarik Softic <tarik.softic@student.tugraz.at>
2019-05-15 15:35:55 +0200

    GAMES-004 color selection UI and logic started [SA,ST]

--- app/src/main/java/com/example/TicTacToe/TicTacToe_ColorSelection_Activity.java
+++ app/src/main/java/com/example/TicTacToe/TicTacToe_ColorSelection_Activity.java
@@ -18,49 +18,89 @@ import com.example.ultimategames.R;
 
 public class TicTacToe_ColorSelection_Activity extends AppCompatActivity implements OnItemSelectedListener{
 
-    private String character_one, character_two;
+    private String character_one, character_two, color_one, color_two;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tic_tac_toe__color_selection_); // Spinner element
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        setContentView(R.layout.activity_tic_tac_toe__color_selection_);
+        Spinner signSpinner = (Spinner) findViewById(R.id.sign_spinner);
+        Spinner colorSpinner = (Spinner) findViewById(R.id.color_spinner);
         Button buttonContinue = (Button) findViewById(R.id.buttonContinue);
 
 
-        spinner.setOnItemSelectedListener(this);
 
-        List<String> categories = new ArrayList<String>();
-        categories.add("X");
-        categories.add("O");
+        List<String> signCategories = new ArrayList<String>();
+        List<String> colorCategories = new ArrayList<String>();
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        colorCategories.add ("Blue");
+        colorCategories.add ("Red");
 
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        signCategories.add("X");
+        signCategories.add("O");
 
-        spinner.setAdapter(dataAdapter);
+        ArrayAdapter<String> signAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, signCategories);
+        signAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        signSpinner.setAdapter(signAdapter);
+
+        ArrayAdapter<String> colorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, colorCategories);
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(colorAdapter);
+
+
+        signSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                character_one = parent.getItemAtPosition(position).toString();
+                character_two = parent.getItemAtPosition((position + 1)%parent.getCount()).toString();
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                character_one = arg0.getItemAtPosition(0).toString();
+                character_two = arg0.getItemAtPosition(1).toString();
+            }
+        });
+
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                color_one = parent.getItemAtPosition(position).toString();
+                color_two = parent.getItemAtPosition((position + 1)%parent.getCount()).toString();
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                color_one = arg0.getItemAtPosition(0).toString();
+                color_two = arg0.getItemAtPosition(1).toString();
+            }
+        });
+
+
+
         buttonContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(TicTacToe_ColorSelection_Activity.this, PvC.class);
                 if(intent.resolveActivity(getPackageManager())!=null){
+
                     String[] strings = {character_one, character_two};
                     intent.putExtra("characters",strings);
+
+                    String[] colors = {color_one, color_two};
+                    intent.putExtra("colors",colors);
+
                     TicTacToe_ColorSelection_Activity.this.startActivity(intent);
                 }
             }
         });
     }
 
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        character_one = parent.getItemAtPosition(position).toString();
-        character_two = parent.getItemAtPosition((position + 1)%parent.getCount()).toString();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-        character_one = arg0.getItemAtPosition(0).toString();
-        character_two = arg0.getItemAtPosition(1).toString();
+
     }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
+    }
 }
 
