03dfad438d09bfb126785bba474e5f43b14beaf1
Markus Schiffermueller <schiffermueller@student.tugraz.at>
2019-06-12 09:53:29 +0200

    GAMES-014 Coding standard [AG,MS]

--- app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
+++ app/src/main/java/com/example/ultimategames/TouchTheBlock/TTB_Activity.java
@@ -51,25 +51,23 @@ public class TTB_Activity extends AppCompatActivity {
         Log.e("Width", "" + width);
         Log.e("height", "" + height);
 
-        final Button btn_block = (Button)findViewById(R.id.bt_block);
-        final Button btn_restart = (Button)findViewById(R.id.bt_Restart);
-        rel_Backround = (RelativeLayout) findViewById (R.id.Rel_Backround);
+        final Button btn_block = (Button) findViewById(R.id.bt_block);
+        final Button btn_restart = (Button) findViewById(R.id.bt_Restart);
+        rel_Backround = (RelativeLayout) findViewById(R.id.Rel_Backround);
 
         btn_block.setBackgroundColor(Color.BLACK);
 
         ViewGroup.LayoutParams params = btn_block.getLayoutParams();
-        params.height = height/2;
-        params.width = width/2;
+        params.height = height / 2;
+        params.width = width / 2;
         btn_block.setLayoutParams(params);
 
         btn_restart.setVisibility(View.INVISIBLE);
 
 
-
         btn_block.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                if(!gameover)
-                {
+                if (!gameover) {
                     time = time_to_react;
                     resizeBtn(v);
                     realignBtn(v);
@@ -80,8 +78,7 @@ public class TTB_Activity extends AppCompatActivity {
 
         btn_restart.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                if(gameover)
-                {
+                if (gameover) {
                     reStart();
                 }
             }
@@ -99,44 +96,45 @@ public class TTB_Activity extends AppCompatActivity {
         timer();
 
     }
-    public void realignBtn(View v){
-        RelativeLayout gameLayout =  findViewById(R.id.Rel_Backround);
-        int Layoutwidth  = gameLayout.getWidth();
+
+    public void realignBtn(View v) {
+        RelativeLayout gameLayout = findViewById(R.id.Rel_Backround);
+        int Layoutwidth = gameLayout.getWidth();
         int Layoutheight = gameLayout.getHeight();
 
-        Button btn = (Button)v;
+        Button btn = (Button) v;
         Random buttonPlace = new Random();
         // Random Number in Bound
-        int buttonX = buttonPlace.nextInt(Layoutwidth-btn.getWidth());
-        int buttonY = buttonPlace.nextInt(Layoutheight-btn.getHeight());
+        int buttonX = buttonPlace.nextInt(Layoutwidth - btn.getWidth());
+        int buttonY = buttonPlace.nextInt(Layoutheight - btn.getHeight());
         btn.setX(buttonX);
         btn.setY(buttonY);
     }
-    public void resizeBtn(View v)
-    {
-        Button btn = (Button)v;
+
+    public void resizeBtn(View v) {
+        Button btn = (Button) v;
 
         ViewGroup.LayoutParams params = btn.getLayoutParams();
-        params.height = btn.getHeight()/2;
-        params.width = btn.getWidth()/2;
+        params.height = btn.getHeight() / 2;
+        params.width = btn.getWidth() / 2;
         btn.setLayoutParams(params);
         testcounter++;
     }
 
-    public void addPoints(){
+    public void addPoints() {
 
         testcounter += 5;
     }
 
-    public void deductPoints(){
+    public void deductPoints() {
 
         testcounter -= 5;
     }
 
-    public void reStart(){
+    public void reStart() {
         //TextView tvScore = (TextView) findViewById(R.id.text_score);
         int global_score = WelcomeScreenActivity.global_score;
-        if(global_score <=10)
+        if (global_score <= 10)
             return;
         else {
             WelcomeScreenActivity.global_score = global_score - 10;
@@ -146,50 +144,45 @@ public class TTB_Activity extends AppCompatActivity {
 
     }
 
-    public void timer(){
+    public void timer() {
 
         countDown = findViewById(R.id.countdown_text);
 
-        new CountDownTimer(30000, 10)
-        {
-            public void onTick(long millisUntilFinished){
+        new CountDownTimer(30000, 10) {
+            public void onTick(long millisUntilFinished) {
                 DecimalFormat df = new DecimalFormat("#.##");
                 countDown.setText(String.valueOf(df.format(time)));
                 time = time - 0.01;
                 double timer_buffer = time;
-                if(Math.abs(timer_buffer-1.0) <= 0.01)
-                {
-                    MediaPlayer ring = MediaPlayer.create(TTB_Activity.this,R.raw.bing_sound);
+                if (Math.abs(timer_buffer - 1.0) <= 0.01) {
+                    MediaPlayer ring = MediaPlayer.create(TTB_Activity.this, R.raw.bing_sound);
                     ring.start();
-                    TextView txtView = (TextView)findViewById(R.id.textView);
+                    TextView txtView = (TextView) findViewById(R.id.textView);
                 }
-                if(time < 0.0)
-                {
+                if (time < 0.0) {
                     gameOver();
                     return;
                 }
             }
-            public  void onFinish(){
+
+            public void onFinish() {
                 countDown.setText("You lost");
             }
         }.start();
     }
 
-    public void gameOver()
-    {
-        final Button btn = (Button)findViewById(R.id.bt_block);
+    public void gameOver() {
+        final Button btn = (Button) findViewById(R.id.bt_block);
         btn.setBackgroundColor(Color.RED);
         deductPoints();
-        TextView txtView = (TextView)findViewById(R.id.textView);
+        TextView txtView = (TextView) findViewById(R.id.textView);
         String hello = "Sorry you lost! Press to try again";
         txtView.setText(hello);
         time = 0.0;
         gameover = true;
 
-        while (restart)
-        {
-            if(txtView.isPressed())
-            {
+        while (restart) {
+            if (txtView.isPressed()) {
                 restart = true;
                 String he = "Fuck yoi snagger";
                 txtView.setText(he);
